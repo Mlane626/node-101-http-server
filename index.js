f440a1323aa2753b16399ed74f6a126315232ba0
Mike L <mike.lane.626@gmail.com>
2023-01-19 16:32:03 -0800

    inital files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code here
+var http = require('http')
+
+var server = http.createServer(function (req, res) {
+    console.log('Recieved' + req.method + ' request for: ' + req.url);
+    res.writeHead(200, {'Content-Type': "text/plain"});
+    // const contentType = request.getHeader('Content-Type');
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost', null, function (){
+console.log('Server is listening on localhost:8080');
+});
+
